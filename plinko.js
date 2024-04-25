3c1c713b5722a9abf1c17a9bd7ed73b45540c2a2
brandonmcadoo <b24mcad@siena.edu>
2024-04-25 17:22:54 -0400

    created basics

--- plinko.js
+++ plinko.js
@@ -3,13 +3,16 @@
 var canvas;
 var gl;
 var vBuffer;
+var cBuffer;
 var ball;
 var peg;
 var triangle_count = 200;
-var cBuffer;
 var maxPoints = 15000000;
 
 var pegs = [];
+var recs = [];
+var displayBall;
+var balls = [];
 
 window.onload = function init() {
     canvas = document.getElementById( "gl-canvas" );
@@ -52,56 +55,78 @@ window.onload = function init() {
     // ball = makeBall(vec2(0.0, .95));
     // peg = makePeg(vec2(0.0, 0.0));
 
-    for(let i = 0; i < 4; i++){
-        let y = 0.5;
-        let c = vec2(i * 0.3, y);
+    for(var i = 0; i < 4; i++){
+        var y = 0.5;
+        var c = vec2(i * 0.3, y);
         pegs.push(makePeg(c));
         c = vec2(i * -0.3, y);
         pegs.push(makePeg(c));
     }
 
-    for(let i = .5; i < 3.5; i++){
-        let y = 0.2;
-        let c = vec2(i * 0.3, y);
+    for(var i = .5; i < 3.5; i++){
+        var y = 0.2;
+        var c = vec2(i * 0.3, y);
         pegs.push(makePeg(c));
         c = vec2(i * -0.3, y);
         pegs.push(makePeg(c));
     }
 
-    for(let i = 0; i < 4; i++){
-        let y = - 0.1;
-        let c = vec2(i * 0.3, y);
+    for(var i = 0; i < 4; i++){
+        var y = - 0.1;
+        var c = vec2(i * 0.3, y);
         pegs.push(makePeg(c));
         c = vec2(i * -0.3, y);
         pegs.push(makePeg(c));
     }
 
-    for(let i = .5; i < 3.5; i++){
-        let y = -0.4;
-        let c = vec2(i * 0.3, y);
+    for(var i = .5; i < 3.5; i++){
+        var y = -0.4;
+        var c = vec2(i * 0.3, y);
         pegs.push(makePeg(c));
         c = vec2(i * -0.3, y);
         pegs.push(makePeg(c));
     }
+
+    for(var i = .5; i < 3.5; i++){
+        var c = vec2(i * 0.3, -.8);
+        recs.push(makeRec(c));
+        c = vec2(i * -0.3, -.8);
+        recs.push(makeRec(c));
+    }
+
+    displayBall = makeBall(vec2(0, .95));
+
+
 
 
 
 	// grid = makeGrid();
-    let vBuffStart;
+    var vBuffStart;
 	gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    for(let i = 0; i < pegs.length; i++){
-        gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (i * pegs[i].positions.length), flatten(pegs[i].positions) );
-        vBuffStart = 8 * (i * pegs[i].positions.length);
+    for(var i = 0; i < pegs.length; i++){
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (i * (pegs[i].positions.length)), flatten(pegs[i].positions) );
     }	
+    for(var i = 0; i < recs.length; i++){
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8 * ((pegs.length * pegs[0].positions.length) + (i * recs[i].positions.length)), flatten(recs[i].positions) );
+    }
+    gl.bufferSubData(gl.ARRAY_BUFFER, 8 * ((pegs.length * pegs[0].positions.length) + (recs.length * recs[0].positions.length)), flatten(displayBall.positions) );
 	
-    let cBuffStart;
+    var cBuffStart;
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    for(let i = 0; i < pegs.length; i++){
-        for(let j = 0; j < pegs[i].positions.length; j++){
-            gl.bufferSubData(gl.ARRAY_BUFFER, 16 * ((i * pegs[i].positions.legnth) + j), flatten(pegs[i].color) );
-            cBuffStart = 16 * (pegs[i].positions.legnth + j);
+    for(var i = 0; i < pegs.length; i++){
+        for(var j = 0; j < pegs[i].positions.length; j++){
+            gl.bufferSubData(gl.ARRAY_BUFFER, 16 * ((j) + (i * pegs[0].positions.length)), flatten(pegs[i].color) );
         }
     }
+    for(var i = 0; i < recs.length; i++){
+        for(var j = 0; j < recs[i].positions.length; j++){
+            gl.bufferSubData(gl.ARRAY_BUFFER, 16 * ((j) + (i * recs[0].positions.length) + (pegs.length * pegs[0].positions.length)), flatten(recs[i].color) );
+        }
+    }
+    for(var i = 0; i < displayBall.positions.length; i++){
+        gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (i + (recs.length * recs[0].positions.length) + (pegs.length * pegs[0].positions.length)), flatten(displayBall.color) );
+    }
+
 	// for (var i = 0; i < grid.positions.length; i++){
 	// 		gl.bufferSubData(gl.ARRAY_BUFFER, 0 + i * 16, flatten(grid.color) );
 	// }
@@ -112,20 +137,20 @@ window.onload = function init() {
 	// }
 	
 
-		canvas.addEventListener("click", (event) =>
-		{
-				//document.getElementById("myAudio").play(); 
-				//shotgunBlast.play();
-				let click = vec2(event.offsetX, event.offsetY);
-				//Offset here????
-				let clippedX = convertX(click[0], canvas);
-				let clippedY = convertY(click[1], canvas);
-				let clippedClick = vec2(clippedX - .05, clippedY + .05);
-				//console.log(clippedClick);
-				getTile(click);
-				render();
-		}
-		);
+		// canvas.addEventListener("click", (event) =>
+		// {
+		// 		//document.getElementById("myAudio").play(); 
+		// 		//shotgunBlast.play();
+		// 		let click = vec2(event.offsetX, event.offsetY);
+		// 		//Offset here????
+		// 		let clippedX = convertX(click[0], canvas);
+		// 		let clippedY = convertY(click[1], canvas);
+		// 		let clippedClick = vec2(clippedX - .05, clippedY + .05);
+		// 		//console.log(clippedClick);
+		// 		getTile(click);
+		// 		render();
+		// }
+		// );
 
 
 
@@ -170,15 +195,35 @@ function makePeg(c) {
 	return {color: col, center:c, radius:r, positions:p};
 }
 
+function makeRec(c){
+    var width = .02;
+
+    var p = [];
+
+    p.push(vec2(c[0] - width, c[1]));
+    p.push(vec2(c[0] + width, c[1]));
+    p.push(vec2(c[0] + width, -1));
+    p.push(vec2(c[0] - width, -1));
+
+    let col = vec4( 0.0, 0.0, 0.0, 1.0 );
+	return {color: col, center:c, width:width, positions:p};
+}
+
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
 
 	gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
 
+    let drawStart;
     for(let i = 0; i < pegs.length; i++){
         gl.drawArrays( gl.TRIANGLE_FAN, i * pegs[i].positions.length, pegs[i].positions.length);
     }
+    for(let i = 0; i < recs.length; i++){
+        gl.drawArrays( gl.TRIANGLE_FAN, (i * recs[i].positions.length) + (pegs.length * pegs[0].positions.length), recs[i].positions.length);
+    }
+    gl.drawArrays( gl.TRIANGLE_FAN, (recs.length * recs[0].positions.length) + (pegs.length * pegs[0].positions.length), displayBall.positions.length);
+
 	
 	
 	requestAnimationFrame(render);
